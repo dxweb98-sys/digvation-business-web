a28f5e7212c2ab6d7519eadc372e203e6eea745b
dxweb98-sys <dxweb98@gmail.com>
2026-09-15 23:14:45 +0700

    feat: surface sale discount and promotion controls

--- apps/operational/src/routes/sell/sell-page.tsx
+++ apps/operational/src/routes/sell/sell-page.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router';
 
 import { ReplatformedPosWorkspace } from '../../features/sell/components/replatformed-pos-workspace';
+import { SaleAdjustmentControls } from '../../features/sell/components/sale-adjustment-controls';
 import { VariantPicker } from '../../features/sell/components/variant-picker';
 import { useCashierTransactionWorkspace } from '../../features/sell/use-cashier-transaction-workspace';
 
@@ -11,6 +12,7 @@ export function SellPage() {
   return (
     <section className="h-full min-h-0 overflow-hidden">
       <ReplatformedPosWorkspace workspace={workspace} />
+      <SaleAdjustmentControls workspace={workspace} />
       {workspace.variantPicker?.context !== 'TRANSACTION_ADJUSTMENT' && workspace.variantPicker ? (
         <VariantPicker
           {...workspace.variantPicker}
